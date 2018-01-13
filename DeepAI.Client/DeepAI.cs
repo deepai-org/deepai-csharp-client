2df99a6df265108ff52d7875cc460189990c2f16
Deep AI <admin@deepai.org>
2018-01-13 01:37:47 -0800

    parse JSON responses into C# classes

--- DeepAI.Client/DeepAI.cs
+++ DeepAI.Client/DeepAI.cs
@@ -8,15 +8,55 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
+using System.Reflection;
+
 
 namespace DeepAI
 {
     /// <summary>
+    /// Represents the status and configuration for a single Realtime video stream.
+    /// </summary>
+    public class RealtimeStream
+    {
+        public int id { get; set; }
+        public string status { get; set; }
+        public string input_type { get; set; }
+        public string output_type { get; set; }
+        public string input_url { get; set; }
+        public string output_url { get; set; }
+        public DateTime finished_at { get; set; }
+        public DateTime started_at { get; set; }
+        public float fps { get; set; }
+        public int height { get; set; }
+        public int width { get; set; }
+    }
+
+    class RealtimeStreamList
+    {
+        public IList<RealtimeStream> streams { get; set; }
+    }
+
+    public class DeepAIError : Exception
+    {
+        public DeepAIError(String s) : base(s)
+        {
+            
+        }
+    } 
+    
+    /// <summary>
     /// Main API Client class - create an instance of the api by calling: DeepAI_API api = new DeepAI_API(apiKey: "...");
     /// </summary>
+
     public class DeepAI_API
     {
         private String apiKey;
+
+        private static JsonSerializerSettings deserializerSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore,
+        };
 
         /// <summary>
         /// Call this to create a new DeepAI_API instance. 
@@ -27,7 +67,8 @@ namespace DeepAI
             this.apiKey = apiKey;
         }
 
-        private Object apiCall(String url_path, String method = "GET", Object postData = null)
+
+        private Object jsonApiCall(String url_path, String method = "GET", Object dataObjectForJson = null)
         {
             // Use TLS 1.2
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -39,10 +80,10 @@ namespace DeepAI
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = method;
 
-            if (postData != null) { 
+            if (dataObjectForJson != null) { 
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    string json = new JavaScriptSerializer().Serialize(postData);
+                    string json = new JavaScriptSerializer().Serialize(dataObjectForJson);
 
                     streamWriter.Write(json);
                 }
@@ -82,36 +123,83 @@ namespace DeepAI
 
             }
 
+        }
 
+
+        private string stringApiCall(String url_path, String method = "GET", Object dataObjectForJson = null)
+        {
+            // Use TLS 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            // uncomment to ignore ssl errors
+            // ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.deepai.org/" + url_path);
+            httpWebRequest.Headers["Api-Key"] = this.apiKey;
+
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = method;
+
+            if (dataObjectForJson != null)
+            {
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = new JavaScriptSerializer().Serialize(dataObjectForJson);
+
+                    streamWriter.Write(json);
+                }
+            }
+
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    return result;
+                }
+            }
+            catch (System.Net.WebException e)
+            {
+                String errMsg = new StreamReader(e.Response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+                if (errMsg != null && errMsg.Length > 0) { 
+                    throw new DeepAIError("Web Error: "+errMsg);
+                }
+                else
+                {
+                    throw new DeepAIError("Web Error: "+e.Message);
+                }
+            }
         }
 
         /// <summary>
         /// Get the full list of your active realtime streams, plus streams started or stopped in the last hour
         /// </summary>
         /// <returns>List of stream info objects</returns>
-        public Dictionary<String, Object> getRealtimeStreams()
+        public IList<RealtimeStream> getRealtimeStreams()
         {
-            return (Dictionary<String, Object>)apiCall(url_path: "realtime-video");
+            RealtimeStreamList list = JsonConvert.DeserializeObject<RealtimeStreamList>(stringApiCall(url_path: "realtime-video"), deserializerSettings);
+            return list.streams;
         }
+
 
         /// <summary>
         /// Get the info for a single realtime stream. Useful for continuously polling the status of a stream.
         /// </summary>
         /// <param name="id">ID of the stream to get</param>
         /// <returns>Stream Info object</returns>
-        public Dictionary<String, Object> getRealtimeStream(int id)
+        public RealtimeStream getRealtimeStream(int id)
         {
-            return (Dictionary<String, Object>)apiCall(url_path: "realtime-video/" + id);
+            return JsonConvert.DeserializeObject<RealtimeStream>(stringApiCall(url_path: "realtime-video/" + id), deserializerSettings);
         }
+
 
         /// <summary>
         /// Abort a single realtime stream.
         /// </summary>
         /// <param name="id">ID of the stream to stop</param>
-        /// <returns>Stream Info object</returns>
-        public Dictionary<String, Object> stopRealtimeStream(int id)
+        /// <returns>RealtimeStream object</returns>
+        public RealtimeStream stopRealtimeStream(int id)
         {
-            return (Dictionary<String, Object>)apiCall(url_path: "realtime-video/" + id + "/stop", method: "POST");
+            return JsonConvert.DeserializeObject<RealtimeStream>(stringApiCall(url_path: "realtime-video/" + id + "/stop", method: "POST"), deserializerSettings);
         }
 
         /// <summary>
@@ -125,8 +213,8 @@ namespace DeepAI
         /// <param name="fps">Frames per second of the input video stream. The output stream will be the same frame rate.</param>
         /// <param name="width">width of the video to process in pixels</param>
         /// <param name="height">height of the video to process in pixels</param>
-        /// <returns>Stream Info object containing URLs which may be used to connect to the stream by sending input and receiving output.</returns>
-        public Dictionary<String, Object> startRealtimeStream(
+        /// <returns>RealtimeStream object containing URLs which may be used to connect to the stream by sending input and receiving output.</returns>
+        public RealtimeStream startRealtimeStream(
             String model,
             String input_type,
             String output_type,
@@ -135,7 +223,7 @@ namespace DeepAI
             int height
             )
         {
-            return (Dictionary<String, Object>)apiCall(url_path: "realtime-video/start", method: "POST", postData: new
+            return JsonConvert.DeserializeObject<RealtimeStream>(stringApiCall(url_path: "realtime-video/start", method: "POST", dataObjectForJson: new
             {
                 model=model,
                 input_type = input_type,
@@ -143,7 +231,8 @@ namespace DeepAI
                 fps=fps,
                 width=width,
                 height=height
-            });
+            }), deserializerSettings);
+
         }
 
         /// <summary>
@@ -154,7 +243,6 @@ namespace DeepAI
         public String objectAsJsonString(Object a){
             return JsonUtility.NormalizeJsonString(JsonConvert.SerializeObject(a));
         }
-
 
     }
 
